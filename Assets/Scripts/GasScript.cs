3e162a019f430cf4f6a3c6a83c218624c9f6408d
popilanitu <artsimenia_run@mail.ru>
2025-03-09 23:26:43 +0300

    переделал кнопку газа

--- Assets/Scripts/GasScript.cs
+++ Assets/Scripts/GasScript.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 public class GasScript : MonoBehaviour
 {
     [SerializeField] private MoveAlongSpline _moveScript;
+    [SerializeField] private AnimationCurve _speedCurve;
     [SerializeField] private float _playerSpeed = 10f;
     [SerializeField] private float _gasTime = 0.1f;
+    [SerializeField, Range(0f, 1f)] private float _maxSpeedEnterTime = 0.2f;
+    [SerializeField, Range(0f, 1f)] private float _maxSpeedExitTime = 0.2f;
 
     private bool _isMoving = false;
+    private float _curGasTime = 0f;
 
     public void OnGas()
     {
-        if (!_isMoving)
+        _isMoving = true;
+        if(_curGasTime / _gasTime > _maxSpeedEnterTime && _curGasTime / _gasTime < _maxSpeedExitTime)
         {
-            _isMoving = true;
-            _moveScript.speed = _playerSpeed;
-            StartCoroutine(GasWait());
+            _curGasTime = _gasTime * _maxSpeedEnterTime;
+        }
+        if(_curGasTime / _gasTime > _maxSpeedExitTime)
+        {
+            _curGasTime = ((_curGasTime - _maxSpeedExitTime) / 1) * _maxSpeedEnterTime;
         }
     }
 
-    IEnumerator GasWait()
+    private void Update()
     {
-        yield return new WaitForSeconds(_gasTime);
-        _moveScript.speed = 0;
-        _isMoving = false;
+        if (_isMoving) _curGasTime += Time.deltaTime;
+        else _curGasTime = 0f;
+        _moveScript.speed = _speedCurve.Evaluate(_curGasTime / _gasTime) * _playerSpeed;
     }
 }
